8ff559fae95f0b97f2ba62b4a84e527ca5b2658f
Al Cheung <cangzan@gmail.com>
2023-11-15 16:35:13 +0800

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+# auto siwtch gh profile
+function __switch_gh_profile --on-variable PWD --description 'switch gh profile on directory change'
+  set current_dir (pwd)
+  set work_dir $HOME/work
+
+  if string match -q "$work_dir*" $current_dir
+    gh profile switch gb
+  else
+    gh profile switch al
+  end
+end
